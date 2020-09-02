3030c0d03ade373cdc10c0df676415ca848d0d0e
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-02 22:39:30 +0200

    add handleClick function

--- 01 - JS Drum Kit/main.js
+++ 01 - JS Drum Kit/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const keyDivs = document.querySelectorAll('.key');
+const soundElements = document.querySelectorAll('audio');
+
+function handleClick(ev) {
+    popUp();
+    playSound();
+}
+
+function popUp() {
+    event.currentTarget.classList.add('playing');
+}
+
+function playSound() {
+    console.log(event.currentTarget.dataset.key);
+}
+
+for (const key of keyDivs) {
+    key.addEventListener('click', handleClick);
+}
