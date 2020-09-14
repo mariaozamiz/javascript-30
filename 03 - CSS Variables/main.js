a9f562d113da6a8b4766396f4e433d0c2981efd4
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-14 17:56:09 +0200

    set project

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function handleKeys(ev) {
+    playSound(ev);
+    highlightDiv(ev);
+}
+
+function playSound(ev) {
+    const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function highlightDiv(ev) {
+    const key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
+    key.classList.add('playing');
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((key) =>
+        key.addEventListener('transitionend', removeTransition)
+    );
+}
+
+function removeTransition(ev) {
+    if (ev.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', handleKeys);
