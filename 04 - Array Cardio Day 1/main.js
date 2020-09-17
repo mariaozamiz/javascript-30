5bafd1ddc238a858d708861a8336f1911defc45d
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-17 20:17:42 +0200

    sort people alphabetically

--- 04 - Array Cardio Day 1/main.js
+++ 04 - Array Cardio Day 1/main.js
@@ -114,8 +114,17 @@ console.table(inventorsByYearsLived);
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//     .map((link) => link.textContent)
+//     .filter((streetName) => streetName.includes('de'));
+
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const peopleByLastName = people.sort((a, b) => (a.people > b.people ? -1 : 1));
+console.log(peopleByLastName);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
