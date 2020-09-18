b5c305160c27e34fbdce0462e0a6e497b991a2f7
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-18 19:40:00 +0200

    add toggle effect

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -6,4 +6,13 @@ function toggleOpen() {
     this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
 panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) =>
+    panel.addEventListener('transitionend', toggleActive)
+);
