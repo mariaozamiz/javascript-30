904bd65a3d9f746fed01fbc643d4831b2726ddae
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-24 14:43:30 +0200

    show search results

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -4,18 +4,30 @@ const endpoint =
     'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 let results = [];
+let searchResults = [];
 
 fetch(endpoint)
     .then((response) => response.json())
     .then((data) => (results = data));
 
 const inputField = document.querySelector('.search');
-inputField.addEventListener('keyup', handleKeyUp);
+inputField.addEventListener('keyup', findResults);
 
-function handleKeyUp() {
+function findResults() {
     const search = inputField.value;
-    const searchResults = results.filter((place) => {
-        return place.city.includes(search);
+    searchResults = results.filter((place) => {
+        return place.city.includes(search) || place.state.includes(search);
     });
     console.log(searchResults);
+    showResults();
+}
+
+function showResults() {
+    const resultsList = document.querySelector('.suggestions');
+    resultsList.innerHTML = '';
+    for (const place of searchResults) {
+        const newLi = document.createElement('li');
+        resultsList.appendChild(newLi);
+        newLi.innerHTML = `${place.city}, ${place.state}`;
+    }
 }
