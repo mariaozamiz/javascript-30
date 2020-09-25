f3b811d7a72ed77085fbae260ed3cef75b74b01c
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-25 11:06:23 +0200

    set draw function

--- 08 - HTML5 Canvas/main.js
+++ 08 - HTML5 Canvas/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const canvas = document.getElementById('draw');
+
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.strokeStyle = '#BADA55';
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 25;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+    console.log(e);
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    context.beginPath();
+    //start from
+    context.moveTo(lastX, lastY);
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    //go to
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
