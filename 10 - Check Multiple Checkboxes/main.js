f11065abba2d8c8b36409fbed7d209845290f857
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-03 19:50:07 +0200

    select and listen to checkboxes

--- 10 - Check Multiple Checkboxes/main.js
+++ 10 - Check Multiple Checkboxes/main.js
@@ -4,3 +4,11 @@
 
 const checkboxes = document.querySelectorAll(`input[type="checkbox"]`);
 console.log(checkboxes);
+
+const checkboxHandler = (ev) => {
+    console.log('hay un cambio en un checkbox ' + ev);
+};
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', checkboxHandler);
+});
