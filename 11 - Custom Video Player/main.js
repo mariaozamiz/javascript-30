095f432811f9c9e7181c721c270d1e9e2da919e5
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-04 14:12:52 +0200

    toggle video playing

--- 11 - Custom Video Player/main.js
+++ 11 - Custom Video Player/main.js
@@ -2,6 +2,7 @@
 
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
+const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const playButton = document.querySelector('.player__button');
 const volumeRange = document.querySelector(`input[name='volume']`);
@@ -14,7 +15,10 @@ function progressHandler() {
 }
 
 function playHandler() {
-    console.log('click en play');
+    video.paused ? video.play() : video.pause();
+
+    //const method = video.paused? 'play' : 'pause';
+    // video[method]();
 }
 
 function volumeHandler() {
@@ -33,7 +37,6 @@ function forward25Handler() {
     console.log('click en forward');
 }
 
-progressBar.addEventListener('change', progressHandler);
 playButton.addEventListener('click', playHandler);
 volumeRange.addEventListener('change', volumeHandler);
 playbackRate.addEventListener('change', playbackHandler);
