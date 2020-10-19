0e321ff8cad2649431f98d5fddc89dd22499c111
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-19 12:01:40 +0200

    key detection

--- 12 - Key Sequence Detection /main.js
+++ 12 - Key Sequence Detection /main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const pressedKeys = [];
+const secretCode = 'raptor';
+
+function keyupHandler(ev) {
+    pressedKeys.push(ev.key);
+    pressedKeys.splice(
+        -secretCode.length - 1,
+        pressedKeys.length - secretCode.length
+    );
+    if (pressedKeys.join('').includes(secretCode)) {
+        console.log('YEAH GRRRRRRRR! 🦖 🦖 🦖 🦖 🦖');
+    }
+}
+
+window.addEventListener('keyup', keyupHandler);
