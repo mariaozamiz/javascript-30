3ef764d6a1717e6853751fab51dc28df37ced4e2
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-09 20:01:14 +0100

    add scroll slide in

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -15,3 +15,24 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+window.addEventListener('scroll', debounce(checkSlide));
+
+function checkSlide(e) {
+    sliderImages.forEach((image) => {
+        //half way through the image
+        const slideInAt =
+            window.scrollY + window.innerHeight - image.height / 2;
+        // bottom of the image
+        const imageBottom = image.offsetTop + image.height;
+        const isHalfShown = slideInAt > image.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            image.classList.add('active');
+        }
+        // } else {
+        //     image.classList.remove('active');
+        // }
+    });
+}
