063e2452ae83ef4a5c4e43bf5f9f1e8489fb8f2d
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-14 21:03:13 +0100

    add  new plates and paint platesList

--- 15 - LocalStorage and Event Delegation/main.js
+++ 15 - LocalStorage and Event Delegation/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const form = document.querySelector('.add-items');
+const platesList = document.querySelector('.plates');
+const inputField = document.querySelector('[name=item]');
+const plates = [];
+
+function addPlateToList(e) {
+    e.preventDefault();
+    const item = {
+        text: inputField.value,
+        done: false,
+    };
+    console.log(item);
+    plates.push(item);
+    paintList();
+    this.reset();
+}
+
+function paintList() {
+    console.log(plates);
+    platesList.innerHTML = plates
+        .map((plate, i) => {
+            return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item-${i}" ${
+                plate.done ? 'checked' : ''
+            } />
+            <label for="item-${i}">${plate.text}</label>
+        </li>
+        `;
+        })
+        .join('');
+}
+
+form.addEventListener('submit', addPlateToList);
