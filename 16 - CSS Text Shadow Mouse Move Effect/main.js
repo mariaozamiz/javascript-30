54a6734b5234a2b6eaf5a879dd7e9c9915e3d995
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-15 23:42:45 +0100

    drop shadows at mousemove

--- 16 - CSS Text Shadow Mouse Move Effect/main.js
+++ 16 - CSS Text Shadow Mouse Move Effect/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const width = hero.offsetWidth;
+const height = hero.offsetHeight;
+const walk = 200;
+
+function moveShadow(e) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width) * walk - walk / 2);
+    const yWalk = Math.round((y / height) * walk - walk / 2);
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 purple, ${yWalk}px ${xWalk}px 0 pink, ${
+        yWalk * -1
+    }px ${xWalk}px 0 navy, ${xWalk * -1}px ${yWalk}px 0 blue`;
+}
+
+hero.addEventListener('mousemove', moveShadow);
