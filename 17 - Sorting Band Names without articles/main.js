6946075d7051c06512f7da777391ec6f78ec401a
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-16 13:59:27 +0100

    sort band names without articles

--- 17 - Sorting Band Names without articles/main.js
+++ 17 - Sorting Band Names without articles/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const bands = [
     'The Plot in You',
     'The Devil Wears Prada',
@@ -14,3 +15,28 @@ const bands = [
     'Anywhere But Here',
     'An Old Dog',
 ];
+
+const bandsList = document.querySelector('#bands');
+let bandsWithoutArticles = [];
+
+createNoArticleBandsList();
+sortList();
+paintList();
+
+function createNoArticleBandsList() {
+    bandsWithoutArticles = bands.map((band) => {
+        return band.replace(/^(a |the |an )/i, '').trim();
+    });
+}
+
+function sortList() {
+    bandsWithoutArticles.sort((a, b) => (a > b ? 1 : -1));
+}
+
+function paintList() {
+    bandsWithoutArticles.forEach((band, i) => {
+        const newBand = document.createElement('li');
+        bandsList.appendChild(newBand);
+        newBand.innerHTML = bandsWithoutArticles[i];
+    });
+}
