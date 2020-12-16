87be1ef12ec7b77d3b29812bad7fe86e72cbf95b
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-16 14:45:39 +0100

    calculate total video time

--- 18 - Tally String Times with Reduce/main.js
+++ 18 - Tally String Times with Reduce/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const timeNodes = [...document.querySelectorAll('[data-time]')];
+
+const seconds = timeNodes
+    .map((node) => node.dataset.time)
+    .map((timeCode) => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return mins * 60 + secs;
+    });
+
+const totalSeconds = seconds.reduce((total, seconds) => total + seconds);
+
+let secondsLeft = totalSeconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(`Total video time is ${hours}:${mins}:${secondsLeft}`);
