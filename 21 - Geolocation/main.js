258c480740488f5a3735aef3cf907e7fd4d3670e
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-21 14:15:37 +0100

    get geolocation and paintspeed

--- 21 - Geolocation/main.js
+++ 21 - Geolocation/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+    (data) => {
+        console.log(data);
+        speed.textContent = Math.round(data.coords.speed);
+        arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    },
+    (error) => {
+        alert('¡Lo siento! No hemos podido acceder a tu localización');
+        console.error(error);
+    }
+);
