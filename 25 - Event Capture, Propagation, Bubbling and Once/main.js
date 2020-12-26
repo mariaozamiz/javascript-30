f5dde9abfa2cfde6ea5ae5cac22b4eaf75f36593
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-26 19:23:24 +0100

    exercise with propagation

--- 25 - Event Capture, Propagation, Bubbling and Once/main.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+    console.log(this);
+    e.stopPropagation(); // stop bubbling
+}
+
+divs.forEach((div) =>
+    div.addEventListener('click', logText, { capture: false, once: true })
+);
