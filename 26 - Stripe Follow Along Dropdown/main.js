4596d87feed16e72557423072d97a9a30e4b5dea
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-27 20:34:36 +0100

    get falldown effect on dropdown

--- 26 - Stripe Follow Along Dropdown/main.js
+++ 26 - Stripe Follow Along Dropdown/main.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const nav = document.querySelector('.top');
+const triggers = document.querySelectorAll('.cool > li');
+const dropdownBackground = document.querySelector('.dropdownBackground');
+
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => {
+        if (this.classList.contains('trigger-enter')) {
+            this.classList.add('trigger-enter-active');
+        }
+    }, 150);
+    dropdownBackground.classList.add('open');
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left,
+    };
+    dropdownBackground.style.setProperty('width', `${coords.width}px`);
+    dropdownBackground.style.setProperty('height', `${coords.height}px`);
+    dropdownBackground.style.setProperty(
+        'transform',
+        `translate(${coords.left}px, ${coords.top}px`
+    );
+}
+
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    dropdownBackground.classList.remove('open');
+}
+
+triggers.forEach((trigger) =>
+    trigger.addEventListener('mouseenter', handleEnter)
+);
+triggers.forEach((trigger) =>
+    trigger.addEventListener('mouseleave', handleLeave)
+);
