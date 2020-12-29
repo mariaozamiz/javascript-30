de76f32ba101149fe443337f2f4f21465627b766
Maria Ozamiz <ozamiz.m@gmail.com>
2020-12-29 19:04:46 +0100

    handle video speed bar

--- 28 - Video Speed Controller UI/main.js
+++ 28 - Video Speed Controller UI/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+function handleMove(e) {
+    const speedBarY = e.pageY - this.offsetTop;
+    const percent = speedBarY / this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const speedBarHeight = Math.round(percent * 100) + '%';
+    const playbackRate = percent * (max - min) + min;
+    bar.style.height = speedBarHeight;
+    bar.textContent = playbackRate.toFixed(2) + 'x';
+    video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove);
